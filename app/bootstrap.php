91808dc26eb2362176ade4724c9161d5be5ab2f7
Mike <mlh1477@gmail.com>
2018-03-03 08:39:03 -0500

    Pull in Illuminate\Database\ and create Capsule. Connect to database

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,15 +1,40 @@
 <?php
    session_start();
 
+   use Illuminate\Database\Capsule\Manager as Capsule;
+
    // Create Slim app
     $app = new \Slim\App([
       'settings' => [
         'displayErrorDetails' => true,
+        'db' => [
+              'driver'    => 'mysql',
+              'host'      => 'localhost',
+              'database'  => 'slimauth',
+              'username'  => 'root',
+              'password'  => '',
+              'charset'   => 'utf8',
+              'collation' => 'utf8_unicode_ci',
+              'prefix'    => '',
+          ]
       ]
-    ]);
+     ]);
 
     // Fetch DI Container
     $container = $app->getContainer();
+
+    //Create Capsule instance
+    $capsule = new Capsule;
+    $capsule->addConnection($container['settings']['db']);
+    // Make this Capsule instance available globally via static methods... (optional)
+    $capsule->setAsGlobal();
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+    $capsule->bootEloquent();
+
+    //Add capsule to the container
+    $container['db'] = function($container) use ($capsule){
+      return $capsule;
+    };
 
     // Register Twig View helper
     $container['view'] = function($container){
