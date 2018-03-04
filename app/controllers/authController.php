a3403b0dc84bbe66e661708c3e01528d4adbaa64
Mike <mlh1477@gmail.com>
2018-03-04 08:49:06 -0500

    Edit sign up form. Create authController->postSignUp method to create new users.

--- app/controllers/authController.php
+++ app/controllers/authController.php
@@ -6,8 +6,19 @@
 
   class authController extends Controller
   {
-    function signUp($request, $response){
+    public function getSignUp($request, $response){
 
       return $this->container->view->render($response, 'signup.twig');
+    }
+
+    public function postSignUp($request, $response){
+
+    $user =  User::create([
+        'email' => $request->getParam('email'),
+        'name' => $request->getParam('name'),
+        'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+      ]);
+
+      return $response->withRedirect($this->container->router->pathFor('home'));
     }
   }
--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->get('/', 'homeController:index');
+$app->get('/', 'homeController:index')->setName('home');
 
-$app->get('/auth/signup', 'authController:signUp')->setName('signup');
-$app->post('/auth/signup', 'authController:signUp');
+$app->get('/auth/signup', 'authController:getSignUp')->setName('signup');
+$app->post('/auth/signup', 'authController:postSignUp');
